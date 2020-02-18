5c8afc2074f23abf722c20cd00a9b7416df834be
KaterynaLis <votetolisitsa@gmail.com>
2020-02-18 21:30:34 +0100

    completed

--- script.js
+++ script.js
@@ -5,13 +5,6 @@ function replaceVirus(element) {
     element.childNodes.forEach(replaceVirus);
   } else if (element.nodeType === Text.TEXT_NODE) {
     if (element.textContent.match(/coronavirus/gi)) {
-      //   const newElement = document.createElement("span");
-      //   newElement.innerHTML = element.textContent.replace(
-      //     /(coronavirus)/gi,
-      //     '<span style = "background-color: black; color: black;">$1<span>'
-      //   );
-      //   element.replaceWith(newElement);
-
       element.parentElement.remove();
     }
   }
