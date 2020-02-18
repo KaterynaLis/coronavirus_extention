c95143b08b9c910dc8b62a3072d1e97c3e25a30c
KaterynaLis <votetolisitsa@gmail.com>
2020-02-18 21:06:41 +0100

    function replaceVirus changed so it will make whole paragraph which includec word coronavirus to  black

--- script.js
+++ script.js
@@ -4,9 +4,13 @@ function replaceVirus(element) {
   if (element.hasChildNodes()) {
     element.childNodes.forEach(replaceVirus);
   } else if (element.nodeType === Text.TEXT_NODE) {
-    element.textContent = element.textContent.replace(
-      /coronavirus/gi,
-      "XXXXXXXXX"
-    );
+    if (element.textContent.match(/coronavirus/gi)) {
+      element.parentElement.style.color = "black";
+      element.parentElement.style.backgroundColor = "black";
+    }
+    // element.textContent = element.textContent.replace(
+    //   /coronavirus/gi,
+    //   "XXXXXXXXX"
+    // );
   }
 }
